9d38e20b1c52a2ff4358563b5f3246da6915e7d1
Namolla <kmc130710@daum.net>
2023-11-21 19:42:06 +0900

    1.0.0

---
+++ src/public/js/app.js
@@ -0,0 +1,57 @@
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+const send_name_e = document.getElementById("send-name");
+const send_text_e = document.getElementById("send-text");
+const send_to_e = document.getElementById("send-to");
+
+socket.addEventListener("open", () => {
+  console.log("서버와 연결됨") 
+})
+
+socket.addEventListener("message", (message) => {
+  console.log("서버에서 메시지를 보냄, 내용 : \"", message.data, "\"")
+  if(message.data === "작성 완료"){
+    alert(`서버에서 메시지를 보냄, 내용 : " ${message.data} "`)
+    send_name_e.value = "";
+    send_text_e.value = "";
+  }else{
+    console.log('a')
+    const serverToDBData = JSON.parse(message.data)
+    
+    serverToDBData.forEach(d => {
+      var mainDiv = document.createElement('div');
+      var dateH3 = document.createElement('h3');
+      var hp = document.createElement('p');
+      var p = document.createElement('p');
+      var sapBong = document.getElementById("bong");
+      var hr = document.createElement("hr");
+      dateH3.innerText = d.date;
+     
+      hp.innerText = "작성자 : " + d.name;
+      
+      p.innerText = d.detail;
+
+      mainDiv.appendChild(dateH3);
+      mainDiv.appendChild(hp);
+      mainDiv.appendChild(p);
+      mainDiv.appendChild(hr);
+      
+      sapBong.appendChild(mainDiv);
+    });
+  }
+  
+})
+
+socket.addEventListener("close", () => {
+  console.log("서버와 연결이 끊김")
+})
+
+
+send_to_e.onclick = function(){
+  if( send_name_e.value !== "" && send_text_e.value !== ""  ){
+    var name_v = send_name_e.value;
+    var text_v = send_text_e.value;
+    socket.send(JSON.stringify({ "name": name_v, "text": text_v }));
+  }
+}
+
---
+++ src/server.js
@@ -0,0 +1,97 @@
+import http from "http";
+import WebSocket from "ws";
+import express from 'express';
+import path from "path";
+// const sqlite3 = require('sqlite3').verbose();
+// const db = new sqlite3.Database("./public/db/index.db");
+
+const app = express();
+
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"))
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log("Listening on http://localhost:3000");
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("브라우저와 연결됨");
+  
+  const sqlite3 = require('sqlite3').verbose();
+  const db_name = path.join(__dirname, "db", "index.db");
+  
+  const db = new sqlite3.Database(db_name, err => {
+    if(err) {
+      return console.error(err.message);
+    }
+    console.log("index.db 데이터 베이스와 연결 성공");
+  });
+
+  const readSQL = "SELECT * FROM bong"
+
+  db.all(readSQL, (err, rows) => {
+    if(err) throw err;
+    const dbReadDataList = [];
+    for(let row of rows){
+      console.log(row)
+      dbReadDataList.push(row);
+    }
+    socket.send(JSON.stringify(dbReadDataList));
+  })
+
+  db.close();
+
+  socket.on("close", () => console.log("브라우저와 연결이 끊김"))
+  socket.on("message", (msg) => {
+    console.log(JSON.parse(msg));
+    var msg2 = JSON.parse(msg);
+
+    const db = new sqlite3.Database(db_name, err => {
+      if(err) {
+        return console.error(err.message);
+      }
+      console.log("index.db 데이터 베이스와 연결 성공");
+    });
+
+
+    function addZero(date) {
+      if (date < 10) {
+        const zeroDate = ('00' + date).slice(-2);
+        return zeroDate;
+      }
+      return date;
+    }
+    function datetime() {
+      var date = new Date();
+      var y = addZero(date.getFullYear());
+      var m = addZero(date.getMonth() + 1);
+      var d = addZero(date.getDate());
+
+      var h = addZero(date.getHours());
+      var tm = addZero(date.getMinutes());
+      var s = addZero(date.getSeconds());
+  
+      return `${y}-${m}-${d} ${h}:${tm}:${s}`;
+    }
+
+
+    const SQLrun = [msg2.name, msg2.text, datetime()];
+    const insertSQL = "INSERT INTO bong (name, detail, date) VALUES (?, ?, ?)";
+
+    db.run(insertSQL, SQLrun, err => {
+      if(err){
+        console.log(err.message);
+      }
+      socket.send("작성 완료");
+    });
+    db.close() 
+  })
+});
+
+
+
+server.listen(3000, handleListen);
